e02a926d59a8034592193494eee8687726c3ca19
xxw <x15237168213@163.com>
2022-09-25 16:08:25 +0800

    简单redis分布式锁和Excel导出

---
+++ src/main/java/com/itself/example/easyExcel/Demo.java
@@ -0,0 +1,60 @@
+package com.itself.example.easyExcel;
+
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.write.style.column.LongestMatchColumnWidthStyleStrategy;
+import com.itself.user.entity.UserPO;
+import com.itself.user.service.UserService;
+import org.apache.commons.lang.StringUtils;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.List;
+
+/**
+ *  EasyExcel数据导出加模板下载
+ *
+ * @Author xxw
+ * @Date 2022/09/25
+ */
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
+class ExportDemo01 {
+    @Resource
+    private HttpServletResponse response;
+    @Resource
+    private UserService userService;
+
+    /**
+     * controller接口按照这个downloadHistoryData接口写
+     */
+    public void downloadHistoryData(String filename) throws IOException {
+        String name  = buildResponse(filename.split("\\."));
+        List<UserPO> list = userService.listAll();
+        EasyExcel.write(response.getOutputStream(), UserPO.class).registerWriteHandler(new LongestMatchColumnWidthStyleStrategy()).sheet(name).doWrite(list);
+    }
+
+    private String buildResponse(String... names) throws UnsupportedEncodingException {
+        //防止中文乱码，和easyExcel没关系
+        response.setContentType("application/vnd.ms-excel");
+        response.setCharacterEncoding("UTF-8");
+        String preName = "数据模板";
+        String end = "xlsx";
+        if (null != names && names.length > 0) {
+            if (!StringUtils.isEmpty(names[0])) {
+                preName = names[0];
+            }
+            if (names.length > 1 && !StringUtils.isEmpty(names[1])) {
+                end = names[1];
+            }
+        }
+        String fileName = URLEncoder.encode(preName, "UTF-8").replaceAll("\\+", "%20");
+        response.setHeader("Content-disposition", "attachment;filename*=UTF-8''" + fileName + "." + end);
+        return preName;
+    }
+}
--- src/main/java/com/itself/example/redisLock/DistributedLockDemo.java
+++ src/main/java/com/itself/example/redisLock/DistributedLockDemo.java
@@ -1,7 +1,13 @@
 package com.itself.example.redisLock;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
 /**
- *  redis 锁使用
+ * redis 锁使用
+ *
  * @Author xxw
  * @Date 2022/09/23
  */
@@ -10,8 +16,33 @@ public class DistributedLockDemo {
 
     }
 }
-class DistributedLockDemo01{
-    public static void main(String[] args) {
 
+
+/**
+ * setNgx 命令进行简单的redis分布式锁设计，防止接口被重复触发
+ */
+class DistributedLockDemo01 {
+    private static final String REDIS_KEY_PREFIX = "service_name";
+    @Resource
+    private  StringRedisTemplate redisTemplate;
+    /**
+     * 通过redis分布式锁进行接口防刷
+     */
+    private  void checkFrequentOpt(Integer id, String methodName) {
+        String key = REDIS_KEY_PREFIX + id + methodName;
+        if (!tryLock(key,String.valueOf(id),1)){ //上锁失败
+            //throw new BusinessException(ReturnCode.FREQUENT_OPT);
+        }
+    }
+    /**
+     *  redis上锁
+     * @param lockId    锁的 key
+     * @param value     锁的值 value
+     * @param second    单位秒
+     * @return          成功/失败
+     */
+    private  boolean tryLock(String lockId, String value, long second) {
+        Boolean result = redisTemplate.opsForValue().setIfAbsent(lockId, value, second, TimeUnit.SECONDS);
+        return null != result && result;
     }
 }
