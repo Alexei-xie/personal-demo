340e574e58ebb27ec24ac6a573f15d6ff0541b32
xxw” <x152371628213@163.com>
2022-11-23 14:32:07 +0800

    bean对象整理

--- src/main/java/com/itself/demo/bean/User.java
+++ src/main/java/com/itself/demo/bean/User.java
@@ -17,46 +17,13 @@ public class User implements Serializable,Cloneable {
 
     private static final long serialVersionUID = 1L;
 
-    private String name = "2";
-    private String age ;
+    private String name = "xxw";
+
+    private String age  = "11";
+
 
     public static void main(String[] args) {
         User user = new User();
-//        user.setName("null")；
-        System.out.println(user.getAge() + "22");
-
-
-        BigDecimal b = new BigDecimal(0);
-        BigDecimal a = new BigDecimal(202.200);
-        DecimalFormat decimalFormat = new DecimalFormat("#0.00");
-
-        System.out.println(decimalFormat.format(b));
-
-
-        System.out.println(b);
-        System.out.println(b.compareTo(BigDecimal.ZERO));
-
-        System.out.println(a.subtract(b));
-        if (a.compareTo(b) > -1) {
-            System.out.println("a大于等于b");
-        }
-
-        if (a.compareTo(b) < 1) {
-            System.out.println("a小于等于b");
-        }
-
-        if(a.compareTo(b) == -1){
-            System.out.println("a小于b");
-        }
-
-        if(a.compareTo(b) == 0){
-            System.out.println("a等于b");
-        }
-
-        if(a.compareTo(b) == 1) {
-            System.out.println("a大于b");
-        }
-
         //测试clone方法
         try {
             User clone = (User)user.clone();
