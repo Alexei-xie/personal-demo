2b48e2d36efbf28546c51b1f7096e350bd2cc172
xxw” <x152371628213@163.com>
2023-03-15 15:52:51 +0800

    添加value注解读取yml配置文件信息

--- src/test/java/com/itself/user/service/impl/UserServiceImplTest.java
+++ src/test/java/com/itself/user/service/impl/UserServiceImplTest.java
@@ -6,6 +6,7 @@ import com.itself.user.entity.UserPO;
 import com.itself.user.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -25,8 +26,12 @@ public class UserServiceImplTest {
     @Resource
     private UserService userService;
 
+    @Value("${demo.test}")
+    private  int count;
+
     @Test
     public void listUser(){
+        System.out.println(count);
         List<UserPO> list = userService.listAll();
         System.out.println("list = " + list);
     }
