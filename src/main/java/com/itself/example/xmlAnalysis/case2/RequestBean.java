8466a0bf8a9a466f89e907b914e83c16a2d30fa8
xxw” <x152371628213@163.com>
2023-03-28 15:07:59 +0800

    将xml转bean案例添加注释

--- src/main/java/com/itself/example/xmlAnalysis/case2/RequestBean.java
+++ src/main/java/com/itself/example/xmlAnalysis/case2/RequestBean.java
@@ -5,9 +5,9 @@ import lombok.Data;
 import javax.xml.bind.annotation.*;
 
 @Data
-@XmlRootElement(name = "soap-env:Envelope")
+@XmlRootElement(name = "soap-env:Envelope")//只要字段中有XmlElement注解就可以不适用这个注解
 @XmlAccessorType(value = XmlAccessType.FIELD)
-@XmlType(propOrder = {"Body"})
+@XmlType(propOrder = {"Body"})//此注解指定有哪些属性，需要跟字段属性保持一致，不用写入下面的链接地址属性
 public class RequestBean {
 
     @XmlAttribute(name="xmlns:soap-env")
@@ -19,7 +19,7 @@ public class RequestBean {
     @XmlAttribute(name="xmlns:xsi")
     protected String xsi="http://www.w3.org/2001/XMLSchema-instance";
 
-    @XmlElement(name="soap-env:Body")
+    @XmlElement(name="soap-env:Body")//这个注解确定xml标签头，跟下面的属性和XmlType注解中叫什么名字没关系
     protected BodyBean Body;
 }
 
