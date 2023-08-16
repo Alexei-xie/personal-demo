cd442df1f8afa63727d8c5699892d17a30825dd7
xw <x152371628213@163.com>
2023-08-16 17:19:07 +0800

    添加线程池使用demo

---
+++ src/main/java/com/itself/common/threadpool/MyThreadFactory.java
@@ -0,0 +1,18 @@
+package com.itself.common.threadpool;
+
+import lombok.AllArgsConstructor;
+
+import java.util.concurrent.ThreadFactory;
+
+@AllArgsConstructor
+public class MyThreadFactory implements ThreadFactory {
+
+  private final ThreadFactory factory;
+
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread thread =factory.newThread(r);
+        thread.setUncaughtExceptionHandler(new GlobalUncaughtExceptionHandler());
+        return thread;
+    }
+}
--- src/main/java/com/itself/user/controller/controller.java
+++ src/main/java/com/itself/user/controller/controller.java
@@ -1,9 +1,13 @@
 package com.itself.user.controller;
 
 import com.itself.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @Author xxw
@@ -11,16 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class controller {
-
+    @Resource
     private UserService userService;
 
-    public controller(UserService userService) {
-        this.userService = userService;
-    }
+    @Resource
+    private ThreadPoolTaskExecutor taskExecutor;
 
     @GetMapping("/test")
-    public void printResult(){
-        System.out.println("result="+ userService.listAll());
+    public void printResult() {
+        log.info("start");
+        System.out.println("result=" + userService.listAll());
+        taskExecutor.execute(() -> {
+            for (int i = 0; i < 10; i++) {
+                log.info(String.valueOf(i));
+                if (i == 5) {
+                    System.out.println(i / 0);
+                }
+            }
+        });
+        log.info("=====");
     }
 }
