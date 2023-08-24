543ea71935739da376f1ca78c54f3b5910f136db
Blandness <x15237168213@163.com>
2023-08-24 22:01:01 +0800

    删除不注释掉的代码

--- src/main/java/com/itself/demo/UserDemo.java
+++ src/main/java/com/itself/demo/UserDemo.java
@@ -13,26 +13,8 @@ import java.util.ArrayList;
 public class UserDemo {
     public static void main(String[] args) throws ParseException {
 
-//        SimpleDateFormat sdf = new SimpleDateFormat("YYYYMMdd");
-//
-//        String format = sdf.format(new Date());
-//        Date date = sdf.parse("20220124");
-//        System.out.println(format);
-//        System.out.println(date);
         User user = new User();
-//        user.setName("张三");
         user.setAge(" 1 ");
-//        List<User> list = new ArrayList<>();
-//        list = null;
-//        UserDTO userDTO = new UserDTO();
-//
-//        NotNull.getList(list).stream().forEach(e->{
-//            BeanUtils.copyProperties(e,userDTO);
-//        });
-//        boolean blank = StringUtils.isBlank(user.getAge());
-//        System.out.println("blank = " + blank);
-//        System.out.println(user.getAge());
-//        String string = ObjectUtils.getDisplayString(user.getAge().trim());
 
         ArrayList<Object> objects = new ArrayList<>();
         System.out.println(objects.size());
