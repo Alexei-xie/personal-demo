7cf10d8121b4d754517bfa1cc9c06ba65e910249
duJi <x15237168213@163.com>
2023-11-19 11:56:14 +0800

    补充配置文件类最佳写法注释

--- src/main/java/com/itself/common/config/TestConfig.java
+++ src/main/java/com/itself/common/config/TestConfig.java
@@ -5,11 +5,14 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * https://www.bilibili.com/video/BV1Rz4y1w7Ri/?spm_id_from=333.1007.top_right_bar_window_history.content.click&vd_source=62b998d0f040e66d18bf1b8728f62ab3
+ * 2:07:00开始看，关于配置文件尽量不要使用@Component类似注解，保证底层无依赖
+ * 可独自写一个配置类返回Bean对象，使用@EnableConfigurationProperties注解，属性里填写对应配置文件类.Class
  * @Author duJi
  * @Date 2023/10/24
  */
 @Data
-@Configuration
+@Configuration//底层组件尽量不要使用这种类似注解
 @ConfigurationProperties(prefix = "aaa")
 public class TestConfig {
 
